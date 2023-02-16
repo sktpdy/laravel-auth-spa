8f718c3b1a8b3487122b1d79efce8d4b79e7383e
sktpy <113280595+sktpy@users.noreply.github.com>
2023-02-16 19:55:33 +0530

    add flash message for account creation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,6 +21,7 @@ class UserController extends Controller
         ]));
         Auth::login($user);
 
-        return redirect()->route('home');
+        return redirect()->route('home')
+            ->with('message', 'Account created🎉');
     }
 }
