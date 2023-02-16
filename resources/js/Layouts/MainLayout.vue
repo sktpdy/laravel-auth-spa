568a27fb78b6715d9f150842531639201da138f8
sktpy <113280595+sktpy@users.noreply.github.com>
2023-02-16 18:24:06 +0530

    add common app layout

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="container md:w-5/6 mx-auto p-5">
+        <div class="mt-4 mb-4 bg-gradient-to-r from-green-400 to-blue-500 p-1 rounded-full shadow-lg shadow-teal-300/30 md:shadow-2xl md:shadow-teal-300/30">
+            <div class="flex items-center justify-between p-4 pl-8 pr-8 bg-gray-800 rounded-full">
+                <Link :href="route('home')" class="text-3xl font-extrabold hover-text-g">&lt;/&gt;</Link>
+                <span class="font-medium text-lg hover-text-g">
+                    <Link href="#">Login/Usr</Link>
+                </span>
+            </div>
+        </div>
+    </nav>
+    <main class="container mx-auto w-full">
+        <slot></slot>
+    </main>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
