26eb615a59a5aef40023810f45799c4f4cccc48f
sktpy <113280595+sktpy@users.noreply.github.com>
2023-02-16 18:11:48 +0530

    add home page

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <Card>
+        <p class="text-xl font-medium mb-2">Dashboard</p>
+        <p class="text-base mb-4 text-gray-300">
+            👤Account Info 🔑Change Password <br>🏃🏼‍♂️Logout
+        </p>
+        <Link href="#" as="button" class="btn-b">Visit</Link>
+    </Card>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import Card from '@/Components/Card.vue'
+</script>
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return inertia('Home');
+})->name('home');
 
